0d1d3f985c6b7685dee625015d326a67ecce27e2
JENIA21 <72182135+JENIA21@users.noreply.github.com>
2021-10-19 21:13:21 +0500

    test!

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("I new branch.");
+        System.out.println("I new branch!");
 
 
         System.out.println("Здравствуйте, введите размерность вашего ввектора.");
